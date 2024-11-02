9a7227ca61b8b344784fa93414607babfcde76ab
octakitten <raspberry2425@gmail.com>
2024-11-02 13:34:58 -0400

    fixing password encryption

--- keygen.sh
+++ keygen.sh
@@ -3,5 +3,4 @@
 # and argument 2 should be a password.
 # the script will encrypt the password and associate it with the 
 # username, misuse it and it will mess up i think.
-openssl pkcs12 -certpbe AES-256-CBC -export -out test_cert.pfx \
-    -inkey private-key-$1.pem -in certificate-$1.pem -passout pass:$2
+openssl genpkey -algorithm RSA -out private-key-$1.pem -aes256 -pass pass:$2
